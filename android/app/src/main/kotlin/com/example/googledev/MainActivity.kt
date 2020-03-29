ee1dacd63b8fcb9b9f4227d9ac75963664bd0723
Waiyan Myothet <menooby1996@gmail.com>
2020-03-29 14:13:35 +0630

    started

---
+++ android/app/src/main/kotlin/com/example/googledev/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.googledev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:googledev/utils/devfest.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> main() async{
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent)
+  );
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitDown,DeviceOrientation.portraitUp]
+  );
+  Devfest.prefs=await SharedPreferences.getInstance();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
